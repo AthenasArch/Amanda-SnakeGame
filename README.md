a83c96ec687389c264b314502bee2c6a3fa73b4e
Leonardo <leonardohilgemberg@gmail.com>
2023-01-17 17:34:14 -0300

    v4.11
    Comentários em todo o código

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Amanda-SnakeGame
+
+Instagram: @athenasarch
+Link di insta: https://instagram.com/athenasarch?igshid=MWI4MTIyMDE=
+
+Olá pessoal, meu nome é leonardo Hilgemberg, o Ellon Musk do Brasil! Haha.
+
+Esse é o jogo da snake, será liberado de forma gratuita para vocês se não me perguntarem 
+sobre o nome do jogo.
--- main.py
+++ main.py
@@ -4,52 +4,60 @@ from pygame.locals import *
 from tkinter import *
 from PIL import ImageTk, Image
 
+# dimensoes da tela
 SCREEN_HEIGHT = 600
 SCREEN_LENGTH = 600
 
+# Nome do jogo, dicas: Ex, irma ou Sogra...
 GAME_NAME = 'Snake Amanda'
 
+# Tamanho dos pixels utilizados (Tambem e o tamanho da caomida da cobra)
 SIZE_PIXEL = 10 
 
 SCORE_ADDER = 3 # Essa constante adiciona diretamente na pontuacao e velocidade da snake
-SCORE_TO_SCARY = 30 # pontos necessarios para assustar o usuario
+SCORE_TO_SCARY = 30 # pontos necessarios mostrar a imagem de susto
 
-START_CLOCK_MIN_PEED = 10
+# velocidade inicial da cobra
+START_CLOCK_MIN_PEED = 10 
 
-# Sem a grade externa
+# Sem a grade externa, caso nao queira utilizar a grade externa, utilize esta funcao
 # def on_grid_random():
 #     x = random.randint(0, 590)
 #     y = random.randint(0, 590)
 #     return (x//SIZE_PIXEL * SIZE_PIXEL, y//SIZE_PIXEL * SIZE_PIXEL)
 
-# Com a grade externa
+# Faz os calculos de geracao da comida da Snke 
 def on_grid_random_withFrame():
+    # gera um valor randomico dentro das margens do tamanho da tela
     x = random.randint(0 + SIZE_PIXEL, 590 - SIZE_PIXEL)
     y = random.randint(0 + SIZE_PIXEL, 590 - SIZE_PIXEL)
     return (x//SIZE_PIXEL * SIZE_PIXEL, y//SIZE_PIXEL * SIZE_PIXEL)
 
+# funcao que trata o colisor
 def colission(c1, c2):
     return (c1[0] == c2[0]) and (c1[1] == c2[1]) 
 
-
+# pontos iniciais
 pontuacao = 0
 
+# Teclas utilizadas
 UP = 0
 RIGHT = 1 
 DOWN = 2
 LEFT = 3
 
+# pegue a tabela de cores RGB para gerenciar cores
 # color       (R, G, B)
 COLOR_WITE  = (255, 255, 255)
 COLOR_RED   = (255, 0, 0)
 COLOR_BLUE   = (0, 0, 255)
 
+# relogio para atualizar a tela
 clock = pygame.time.Clock()
 
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_LENGTH))
-pygame.display.set_caption(GAME_NAME)
-
+pygame.init() # inicia o pygame
+screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_LENGTH)) # inicia a tela com os tamanhos setados
+pygame.display.set_caption(GAME_NAME) # nome do jogo
 
 # definimos a imagem de susto
 def scary():    
@@ -76,8 +84,6 @@ def scary():
     label.pack()
 
     win.mainloop()
-    
-
 
 myFrame = pygame.Surface((SIZE_PIXEL, SIZE_PIXEL))
 myFrame.fill(COLOR_BLUE) 
@@ -92,21 +98,26 @@ snake = [(200, 200), (210, 200), (220, 200)]
 snake_skin = pygame.Surface((SIZE_PIXEL, SIZE_PIXEL))
 snake_skin.fill(COLOR_WITE)
 
-# apple_pos = on_grid_random()
-apple_pos = on_grid_random_withFrame()
-apple = pygame.Surface((SIZE_PIXEL, SIZE_PIXEL))
-apple.fill(COLOR_RED)
+# rat_pos = on_grid_random()
+rat_pos = on_grid_random_withFrame()
+rat = pygame.Surface((SIZE_PIXEL, SIZE_PIXEL))
+rat.fill(COLOR_RED)
 
+# direcao de inicio do jogo
 my_direction = RIGHT
 
 # makeLargeFrame()
 
 while True:
     clock.tick(pontuacao + START_CLOCK_MIN_PEED)
+    
+    # habilita tratamento de eventos do telcado
     for event in pygame.event.get():
+        # evento de saida do jogo
         if event.type == QUIT:
             pygame.quit()
 
+        # trata os eventos das telcas direcionais
         if event.type == KEYDOWN:
             if event.key == K_UP:
                 my_direction = UP
@@ -117,11 +128,10 @@ while True:
             if event.key == K_RIGHT:
                 my_direction = RIGHT
         
-        # if event.type == KEYLE
-        # if event.type == KEYDOWN
-    if colission(snake[0], apple_pos):
-        # apple_pos = on_grid_random()
-        apple_pos = on_grid_random_withFrame()
+    # se a cobra colidir com o rato (Comer o rato)
+    if colission(snake[0], rat_pos):
+        # rat_pos = on_grid_random()
+        rat_pos = on_grid_random_withFrame()
         snake.append((0,0))
         pontuacao = pontuacao + SCORE_ADDER
         print("ponto = ", pontuacao)
@@ -132,10 +142,11 @@ while True:
         scary()
         pygame.quit()
 
-    # # Faz a cobra aumentar o tamanho
+    # Faz a cobra aumentar o tamanho
     for i in range(len(snake) - 1, 0, -1):
         snake[i] = (snake[i - 1][0], snake[i - 1][1])
 
+    # as condicionais abaixo fazem com que a matriz da cobra se movimente nas posicoes
     if my_direction == UP:
         snake[0] = (snake[0][0], snake[0][1] - SIZE_PIXEL)
     if my_direction == DOWN:
@@ -146,9 +157,9 @@ while True:
         snake[0] = (snake[0][0] - SIZE_PIXEL, snake[0][1])
 
     screen.fill((0, 0, 0))
-    screen.blit(apple, apple_pos)
+    screen.blit(rat, rat_pos)
     
-    # janela em x sup
+    # se colidir na janela em x sup 
     for frame_pos in range(0, SCREEN_LENGTH, 10):
         frame_x_y_pos = (frame_pos, 0)
         screen.blit(myFrame, frame_x_y_pos)
@@ -156,21 +167,23 @@ while True:
             pygame.quit()
             print("SE FODEU")
 
-    # janela em x inf
+    # se colidir na janela em x inf
     for frame_pos in range(0, SCREEN_LENGTH, 10):
         frame_x_y_pos = (frame_pos, SCREEN_LENGTH-10)
         screen.blit(myFrame, frame_x_y_pos)
         if colission(snake[0], frame_x_y_pos):
             pygame.quit()
             print("SE FODEU")
-    # janela em y esq
+
+    # se colidir na janela em y esq
     for frame_pos in range(0, SCREEN_HEIGHT, 10):
         frame_x_y_pos = (0, frame_pos)
         screen.blit(myFrame, frame_x_y_pos)
         if colission(snake[0], frame_x_y_pos):
             pygame.quit()
             print("SE FODEU")
-    # janela em y dir
+
+    # se colidir na janela em y dir
     for frame_pos in range(0, SCREEN_HEIGHT, 10):
         frame_x_y_pos = (SCREEN_HEIGHT - 10, frame_pos)
         screen.blit(myFrame, frame_x_y_pos)
@@ -178,7 +191,9 @@ while True:
             pygame.quit()
             print("SE FODEU")
 
+    # isso aqui atualiza a posicao da cobra na tela
     for pos in snake:
         screen.blit(snake_skin, pos)
 
+    # isso aqui atualiza a tela
     pygame.display.update()
